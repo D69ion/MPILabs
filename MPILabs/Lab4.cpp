8e6c8d1064fe4bf384ededabc8d1fb09987132c9
dmitriy <dmitriy.shtele@gmail.com>
2019-04-22 19:19:08 +0400

    1

--- MPILabs/Lab4.cpp
+++ MPILabs/Lab4.cpp
@@ -14,29 +14,30 @@ int main(int argc, char *argv[])
 	MPI_Get_processor_name(name, &resultlen);
 
 	int sizeArr = 0;
-	char *send = new char[sizeArr];
-	char *req = new char[sizeArr];
+	char *buffer = new char[30];
 
 	if (rank == 0) {
-		cout << "Enter array size: ";
-		cin >> sizeArr; cout << endl;
-		for (int i = 0; i < size; i++)
-		{
-			send[i] = 147;
-		}
+		cout << "Enter text to array: " << endl;
+		cin >> buffer;
 		double starttime = MPI_Wtime();
-		MPI_Send(send, sizeArr, MPI_CHAR, rank + 1, rank + 1, MPI_COMM_WORLD);
-		cout << "0  Sending data to loop. Sending time: " << MPI_Wtime() - starttime << endl;
-		MPI_Recv(req, sizeArr, MPI_CHAR, size - 1, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-		cout << "Process nummber: " << rank << " receive data: " << req << " from process: " << size - 1
+		MPI_Send(buffer, 30, MPI_CHAR, rank + 1, rank + 1, MPI_COMM_WORLD);
+		cout << "Sending data to loop. Sending time: " << MPI_Wtime() - starttime << endl;
+		MPI_Recv(buffer, 30, MPI_CHAR, size - 1, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+		cout << "Process nummber: " << rank << " receive data: " << buffer << " from process: " << size - 1
 			<< ". Total running time: " << MPI_Wtime() - starttime << " sec" << endl;
 	}
 	else {
 		double starttime = MPI_Wtime();
-		MPI_Recv(req, sizeArr, MPI_CHAR, rank - 1, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-		cout << "Process nummber: " << rank << " receive data: " << req << " from process: " << size - 1 << endl;
-		MPI_Send(req, sizeArr, MPI_CHAR, rank + 1, rank + 1, MPI_COMM_WORLD);
-		cout << rank + "  Sending data to: " << rank + 1 << " process from: " << rank << " process. Sending time: " << MPI_Wtime() - starttime << " sec" << endl;
+		MPI_Recv(buffer, 30, MPI_CHAR, rank - 1, rank, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+		cout << "Process nummber: " << rank << " receive data: " << buffer << " from process: " << rank - 1 << endl;
+		if (rank == size - 1) {
+			MPI_Send(buffer, 30, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+			cout <<"Sending data to: " << 0 << " process from: " << rank << " process. Sending time: " << MPI_Wtime() - starttime << " sec" << endl;
+		}
+		else {
+			MPI_Send(buffer, 30, MPI_CHAR, rank + 1, rank + 1, MPI_COMM_WORLD);
+			cout << "Sending data to: " << rank + 1 << " process from: " << rank << " process. Sending time: " << MPI_Wtime() - starttime << " sec" << endl;
+		}
 	}
 
 	MPI_Finalize();
